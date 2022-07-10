e0055579d951f18db0d843a0e9055a3046108e27
parzuko <jivansh17@gmail.com>
2022-07-11 02:44:09 +0530

    :sparkles: Create nav

---
+++ components/FolowedByNav.tsx
@@ -0,0 +1,7 @@
+const FollowedByNav = () => (
+  <nav className="font-august flex items-center p-4 text-3xl md:p-8 md:text-5xl">
+    <div className="mr-3 h-8 w-8 rounded-lg bg-white bg-gradient-to-b from-blue-400 to-violet-600 md:h-12 md:w-12"></div>
+    Followed By
+  </nav>
+);
+export { FollowedByNav };
