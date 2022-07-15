0645f00d6687dfbd836c6efc1bf53d2142f208fa
parzuko <jivansh17@gmail.com>
2022-07-16 02:10:40 +0530

    :lipstick: Reduce nav font size

--- components/FolowedByNav.tsx
+++ components/FolowedByNav.tsx
@@ -1,6 +1,6 @@
 const FollowedByNav = () => (
-  <nav className="font-august flex items-center p-4 text-3xl md:p-8 md:text-5xl">
-    <div className="mr-3 h-8 w-8 rounded-lg bg-white bg-gradient-to-b from-blue-400 to-violet-600 md:h-12 md:w-12"></div>
+  <nav className="font-august flex items-center p-4 text-3xl sm:p-6 sm:text-4xl md:p-8 md:text-5xl">
+    <div className="mr-3 h-8 w-8 rounded-lg bg-white bg-gradient-to-b from-blue-400 to-violet-600 sm:h-10 sm:w-10 md:h-12 md:w-12"></div>
     Followed By
   </nav>
 );
