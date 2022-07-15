e4b4ff4b6af92e39caadf32b8018d745a47de32a
parzuko <jivansh17@gmail.com>
2022-07-16 03:13:33 +0530

    :sparkles: Create GlowingDemoGIF

---
+++ components/GlowingDemoGIF.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const GlowingDemoGIF = () => (
+    <div className="relative mt-12 select-none md:mt-24 lg:mt-20">
+        <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 opacity-75 blur-md"></div>
+        <Image
+            className="relative rounded-lg"
+            src="/assets/followedby.gif"
+            alt="Followed By Extension"
+            width={840}
+            height={462}
+        />
+    </div>
+);
+export { GlowingDemoGIF };
