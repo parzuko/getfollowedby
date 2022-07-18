74228f182b23e2f8c356ca7b6df7499fa833c69b
parzuko <jivansh17@gmail.com>
2022-07-19 02:11:17 +0530

    :sparkles: Create landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,40 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import { CTAButtons } from "../components/CTAButtons";
+import { FollowedByNav } from "../components/FolowedByNav";
+import { Footer } from "../components/Footer";
+import { GlowingDemoGIF } from "../components/GlowingDemoGIF";
+import { ProductFeatures } from "../components/ProductFeatures";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <main className="flex min-h-screen flex-col text-white">
+            <Head>
+                <title>Followed By</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+            <FollowedByNav />
+            <section className="mx-auto mt-10 flex max-w-4xl flex-col justify-center px-8 text-center lg:mt-12">
+                
+                <h2 className="text-4xl font-extrabold tracking-tight md:text-6xl lg:text-7xl">
+                    {/* Your AI pair programmer */}
+                    Your mutual programmer
+                </h2>
+                <p className="font-alliance text-md mt-4 px-2 text-stone-400 md:px-14 md:text-lg lg:text-xl">
+                    {/* GitHub Copilot uses the OpenAI Codex to suggest code and entire functions in real-time, right from your editor. */}
+                    Followed By uses the GitHub Public API to display mutual
+                    followers for every profile you visit in real-time, right
+                    from your browser.
+                </p>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+                <CTAButtons />
+                <GlowingDemoGIF />
+            </section>
+            <Footer/>
+        </main>
+    );
+};
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
