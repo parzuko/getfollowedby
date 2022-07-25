85c6d04dacf0a07bc5dbfb1a2db6c136254daef4
parzuko <jivansh17@gmail.com>
2022-07-26 02:56:06 +0530

    :busts_in_silhouette: Link footer credits

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,14 @@ import { FollowedByNav } from "./FolowedByNav";
 
 const Footer = () => (
     <footer className="bg-[#13171d] mt-12 flex justify-between items-center py-6 px-6">
-        <FollowedByNav isFooter/>
-        <span className="font-alliance text-slate-300 text-md md:text-lg">Made for the vibes by @parzuko</span>
+        <FollowedByNav isFooter />
+        <span className="font-alliance text-slate-300 text-md md:text-lg">
+            Made for fun by{" "}
+            <a href="https://twitter.com/parzuko" className="hover:text-slate-400" target="_blank">
+                {" "}
+                @parzuko{" "}
+            </a>
+        </span>
     </footer>
 );
 export { Footer };
